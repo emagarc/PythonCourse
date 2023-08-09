6ab1de18faa65688300fff1af7e4290ffa440bcb
Emanuel Garcia <wemagw@gmail.com>
2023-08-09 14:17:53 -0300

    ejercicios_2.3

---
+++ ejercicios_2/ejercicios_2.3.py
@@ -0,0 +1,16 @@
+# Creando una funcion que muestre la serie fibonacci entre 0 y el nnumero dado.
+
+def fibonacci(num):
+    a, b = 0, 1
+    fibonacci_lista = [0]
+    for i in range(num):
+        if b > num:
+            return fibonacci_lista
+        else:
+            fibonacci_lista.append(b)
+            a, b = b, a+b
+    return fibonacci_lista
+
+
+resultado = fibonacci(20)
+print(resultado)
