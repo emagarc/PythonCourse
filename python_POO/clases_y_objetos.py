544736c6d6e03675beb18d5e62b56f0b9e3dc579
Emanuel Garcia <wemagw@gmail.com>
2023-08-24 20:08:49 -0300

    ejercicios 1 POO

--- python_POO/clases_y_objetos.py
+++ python_POO/clases_y_objetos.py
@@ -3,7 +3,18 @@ class Celular:
         self.marca = marca
         self.modelo = modelo
         self.camara = camara
+# Siempre se les pasa el parametro self a los metodos de un clase
+
+    def llamar(self):
+        print(f"Estas haciendo un llamado desde un {self.modelo}")
+
+    def cortar(self):
+        print(f"Cortaste la llamada desde un {self.modelo}")
 
 
 celular1 = Celular("Samsung", "S23", "48MP")
 celular2 = Celular("Apple", "Iphone 15 Pro", "96MP")
+
+# Llamando a los métodos de los celulares, instancias de la clase Celular
+celular1.llamar()
+celular2.cortar()
