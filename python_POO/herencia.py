72d34fe2174672e83fffb1011f1fb6ccb2f5ab71
Emanuel Garcia <wemagw@gmail.com>
2023-08-28 23:26:07 -0300

    Herencia multiple p1

--- python_POO/herencia.py
+++ python_POO/herencia.py
@@ -29,3 +29,45 @@ emanuel = Empleado("Emanuel", 29, "argentino", "Programador", 100000)
 
 print(emanuel.nacionalidad)
 emanuel.hablar()
+
+# Herencia multiple:
+
+# Creamos una nueva clase: Artista
+
+
+class Artista:
+    def __init__(self, habilidad):
+        self.habilidad = habilidad
+
+    def mostrar_habilidad(self):
+        return self.habilidad
+
+
+# Ahora creamos la clase Empleado Artista
+
+class EmpleadoArtista(Persona, Artista):
+    def __init__(self, nombre, edad, nacionalidad, habilidad, salario, empresa):
+        Persona.__init__(self, nombre, edad, nacionalidad)
+        Artista.__init__(self, habilidad)
+        self.salario = salario
+        self.empresa = empresa
+
+    # Usando self definimos un metodo que se llama igual que un metodo heredado
+
+    def mostrar_habilidad(self):
+        return "No tengo jaja"
+
+    # Usando super llamamos al metodo heredado de la clase Artista
+
+    def presentarse(self):
+        return f"Hola, soy {self.nombre} y mi habilidad es: {super().mostrar_habilidad()} y trabajo en: {self.empresa}"
+
+# Creamos un empleado artista:
+
+
+javier = EmpleadoArtista("Javier", 51, "argentino", "Pintor", 1000, "INCA")
+
+# Llamamos a sus metodos.
+
+print(javier.mostrar_habilidad())
+print(javier.presentarse())
