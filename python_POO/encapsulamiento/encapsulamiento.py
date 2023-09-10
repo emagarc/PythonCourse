b64f05066423cb79cb39fd1a610eaba64ea5d987
Emanuel Garcia <wemagw@gmail.com>
2023-09-09 22:05:50 -0300

    Getters&Setters

--- python_POO/encapsulamiento/encapsulamiento.py
+++ python_POO/encapsulamiento/encapsulamiento.py
@@ -1,28 +1,39 @@
-# En python para declarar privado un atributo o valor usamos un _ al principio del atributo.
+# En Python, para declarar un atributo como privado, utilizamos un guión bajo (_) al principio del nombre del atributo.
 
 class MIClase:
     def __init__(self):
-        self._atributo_privado = "Valor"
+        self._atributo_privado = "Valor"  # Atributo privado con guión bajo al inicio
 
-# Ahora si queremos llevar el atributo a un nivel mayor de encampsulamiento utilizamos el doble guion __
+# Ahora, si queremos llevar el atributo a un nivel mayor de encapsulamiento, utilizamos el doble guion bajo (__).
 
 
 class MIClase2:
     def __init__(self):
+        # Atributo altamente privado con doble guion bajo al inicio
         self.__atributo_privado = "Valor"
 
     def __hablar(self):
-        print("Hola como estas!S")
+        # Método privado con doble guion bajo al inicio
+        print("Hola, ¿cómo estás?")
 
-# Ahora intentamos acceder al metodo hablar.
-# Que deberia comentar aqui? se puede acceder a este metodo encapsulado?
+# Ahora intentamos acceder al método "hablar" y al atributo "__atributo_privado".
+#¿Se puede acceder a estos elementos encapsulados?
 
 
+# Para el primer objeto (MIClase), podemos acceder al atributo privado con un guion bajo:
 objeto = MIClase()
 print(objeto._atributo_privado)
 
-# Creamos otros objetos instancias de MIClase2
-# Que deberia comentar aqui? se puede acceder a este metodo encapsulado?
-
+# Para el segundo objeto (MIClase2), intentamos acceder al atributo altamente privado con doble guion bajo.
+# Esto generará un error ya que Python realiza una modificación del nombre del atributo para hacerlo más privado.
+# Por lo tanto, deberíamos comentar que este acceso generará un error:
 objeto2 = MIClase2()
 print(objeto2.__atributo_privado)
+
+# Creamos otros objetos instancias de MIClase2
+# ¿Qué deberíamos comentar aquí? ¿Se puede acceder al método encapsulado?
+
+
+# Intentar acceder al método "__hablar" generará un error similar al del atributo "__atributo_privado".
+# Deberíamos comentar que este acceso también generará un error:
+objeto2.__hablar()
---
+++ python_POO/encapsulamiento/guetters_y_setters.py
@@ -0,0 +1,26 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self._nombre = nombre
+        self._edad = edad
+
+    def get_nombre(self):
+        return self._nombre
+
+    def set_nombre(self, new_nombre):
+        self._nombre = new_nombre
+
+# Creamos el objeto de la clase Persona
+
+
+emanuel = Persona("Emanuel", 29)
+
+# Accedemos a su propiedad privada mediante su getter
+
+nombre = emanuel.get_nombre()
+print(nombre)
+
+# Seteamos su nuevo nombre
+
+emanuel.set_nombre("Andres")
+nombre = emanuel.get_nombre()
+print(nombre)
